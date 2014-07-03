4a03ce238bdb77660f55ec11e261e2d6d835d960
JuneHyeon Bae <devunt@gmail.com>
2014-07-03 15:11:29 +0900

    Move scss to up, and add sudo prefix

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /imgtl
 sudo -u imgtl git pull
+sudo -u imgtl scss --style compressed --update static/scss:static/css
 pip install --upgrade -r requirements.txt
-scss --style compressed --update static/scss:static/css
 service uwsgi restart
